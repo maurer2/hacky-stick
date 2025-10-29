aa590af290c84d8eefe6465edfee5912d7e322cc
Martin <mr.mmaurer@gmail.com>
2025-10-30 00:17:01 +0100

    Added example for scroll-state queries

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,96 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
-  import Counter from "./lib/Counter.svelte";
+  import { z } from 'zod';
+
+  import bookJSON from './assets/book.json' with { type: 'json' };
+
+  export const bookSchema = z.strictObject({
+    title: z.string(),
+    author: z.string(),
+    chapters: z.array(
+      z.object({
+        title: z.string(),
+        content: z.string(),
+      }),
+    ),
+  });
+  const { data: book } = bookSchema.safeParse(bookJSON);
+  const isSupportingScrollStateQueries = CSS.supports('container-type: scroll-state');
 </script>
 
+<header class="masthead">
+  <h1 class="title">Scroll state queries</h1>
+</header>
+
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  {#if !isSupportingScrollStateQueries}
+    <div role="alert" class="alert-message">
+      Scroll state queries are not supported in this browser.
+    </div>
+  {/if}
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+  {#each book?.chapters as chapter}
+    <section class="chapter">
+      <header class="chapter-header">
+        <h2 class="chapter-title">{chapter.title}</h2>
+      </header>
+      <p class="chapter-content">{chapter.content}</p>
+    </section>
+  {:else}
+    <p>No data found.</p>
+  {/each}
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .masthead {
+    display: flow-root;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  .title {
+    margin-block-start: 0;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+
+  main {
+    counter-reset: section;
   }
-  .read-the-docs {
-    color: #888;
+
+  .alert-message {
+    margin-block-end: 1rem;
+    color: #dcdcaa;
+  }
+
+  .chapter {
+    counter-increment: section;
+  }
+
+  .chapter-header {
+    position: sticky;
+    inset-block-start: 0;
+    container-type: scroll-state;
+    container-name: chapter-header;
+  }
+
+  .chapter-title {
+    display: flex;
+    margin-block: 0;
+    padding-block: 0.5rem;
+    padding-inline: 0.5rem;
+    gap: 1rem;
+    font-size: 1.25rem;
+    background: #267f99;
+
+    /* :stuck */
+    @container chapter-header scroll-state(stuck: top) {
+      background: #098658;
+    }
+
+    &:after {
+      content: counter(section);
+      margin-inline-start: auto;
+    }
+  }
+
+  .chapter-content {
+    margin-block-start: 1rem;
+    margin-block-end: 2rem;
   }
 </style>
